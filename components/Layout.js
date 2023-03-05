c24f2b14437595b18a7a151936dfe0ec75e699b2
Ben West <benwest0291@outlook.com>
2023-03-05 16:11:55 +0000

    removed blue link on contact number

--- components/Layout.js
+++ components/Layout.js
@@ -11,6 +11,7 @@ const Layout = ({ children }) => {
             <title>Dawn Driving Tuition</title>
             <meta name="description" content="Female Driving Instructor, Individually Tailored Driving Lessons for pupils of all ages and abilities" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="format-detection" content="telephone=no"></meta>
             <link rel="icon" href="/lplate.png" />
           </Head>
           <main className="main__content">
