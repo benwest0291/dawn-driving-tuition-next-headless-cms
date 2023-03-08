e8d1f9de1f426003c694845b8aec03248bb76aae
Ben West <benwest0291@outlook.com>
2023-03-08 19:08:40 +0000

    image not pulling through

--- components/Text/Text.js
+++ components/Text/Text.js
@@ -9,7 +9,7 @@ const Text = ({ info }) => {
     <div className="text">
       <h2 className="mb-4 mt-4">{pageTitle}</h2>
       {documentToReactComponents(bodyText)}
-      <Image alt="driving and vehicle standards agency" src="/DVSA.JPG" width={160} height={100} className="mt-1"/>
+      <Image alt="driving and vehicle standards agency" src="/DVSA.jpg" width={160} height={100} className="mt-1" />
     </div>
   )
 }
